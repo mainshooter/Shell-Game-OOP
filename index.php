8b24b6ff063b60623affd639e0af696349e1b1ff
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-27 14:01:17 +0100

    Intial files

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
 
   require_once 'Ball.class.php';
   require_once 'Player.class.php';
-  require_once 'Cub.class.php';
+  require_once 'Cup.class.php';
 
   $Ball = new Ball('red');
   $Player = new Player('Peter', 100);
 
+  $Cup1 = new Cup('yellow', 'plastic');
+  $Cup2 = new Cup('yellow', 'plastic');
+  $Cup3 = new Cup('yellow', 'plastic');
 ?>
