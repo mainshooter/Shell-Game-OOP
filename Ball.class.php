95631ded85712480073f289f4640bc579f002570
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-27 13:58:47 +0100

    Some tweaks

--- Ball.class.php
+++ Ball.class.php
@@ -2,13 +2,16 @@
 
   class Ball {
     private $color;
+    private $type;
 
     /**
      * Runs when the class is istanceerd and sets the color
      * @param [string] $color [Our color]
+     * @param [string] $type [The material of the cub]
      */
-    public function __construct($color) {
+    public function __construct(string $color, string $type) {
       $this->color = $color;
+      $this->type = $type;
     }
   }
 
