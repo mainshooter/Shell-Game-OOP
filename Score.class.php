91e25202d3045aeaf776547e8c1a9657398825a7
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-30 13:23:52 +0100

    Renamed Game class to Score class

--- Score.class.php
+++ Score.class.php
@@ -1,6 +1,6 @@
 <?php
 
-  class Game {
+  class Score {
     private $amountPerGame;
 
     /**
--- index.php
+++ index.php
@@ -2,20 +2,20 @@
   session_start();
   include 'view.php';
 ?>
-<ol>
+<!-- <ol>
   <li>5</li>
   <li>3</li>
-</ol>
+</ol> -->
 <?php
 
   require_once 'Ball.class.php';
   require_once 'Player.class.php';
   require_once 'Cup.class.php';
-  require_once 'Game.class.php';
+  require_once 'Score.class.php';
 
-  $Game = new Game();
+  $Score = new Score();
   $Ball = new Ball('red');
-  $Player = new Player('Peter', 100);
+  $Player = new Player('Peter', $Score->getAmount());
   $Cups = [
     new Cup('yellow', 'plastic'),
     new Cup('yellow', 'plastic'),
@@ -32,7 +32,12 @@
     generateField();
     if ($randomNummer != $_GET['choose']) {
       // No eachal to each other one point off!
-      $Game->setAmount($Game->getAmount()-1);
+      $Score->setAmount($Score->getAmount()-1);
+    }
+    else if (ISSET($_GET['reset'])) {
+      $Score->setAmount(20);
+      header("Location: index.php");
+
     }
   }
 
@@ -44,7 +49,7 @@
     global $Player;
     global $Ball;
     global $randomNummer;
-    global $Game;
+    global $Score;
 
     echo '<div class="cups">';
 
@@ -62,13 +67,13 @@
     }
     echo '</div><div class="clear"></div>';
       echo $Player->show();
-      echo "Points: " . $Game->getAmount();
+      echo "Points: " . $Score->getAmount();
   }
 
   ?>
   <a href="?choose=1"><button type='button'>1</button></a>
   <a href="?choose=2"><button type='button'>2</button></a>
   <a href="?choose=3"><button type='button'>3</button></a>
-  <a href="?">Herstart</a>
+  <a href="?restart=true">Herstart</a>
 
 </body>
